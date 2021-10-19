f9e09837fd7af1b328409b6041fa4548e18edc00
Jeevithasri17 <jeevithasri.kanini@kanini.com>
2021-10-19 21:45:07 +0530

    first commit

---
+++ VendorAPI/Controllers/VendorController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendorAPI.CTSModel;
+
+namespace VendorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly NewspaperContext db;
+        public VendorController(NewspaperContext _db)
+
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllVendor()
+        {
+            return Ok(await db.Vendortbls.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddVendor(Vendortbl c)
+        {
+            db.Vendortbls.Add(c);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Login")]
+
+        public IActionResult VendorLogin(Vendortbl v)
+        {
+            Vendortbl vendor = (from i in db.Vendortbls where i.UserName == v.UserName && i.Password == v.Password select i).FirstOrDefault();
+            return Ok(vendor);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateVendor(int id, Vendortbl c)
+        {
+            //a = await db.Accounts.FindAsync(id);
+            db.Entry(c).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteVendor(int id)
+        {
+            Vendortbl c = db.Vendortbls.Find(id);
+            db.Vendortbls.Remove(c);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Vendortbl>> GetVendorById(int id)
+        {
+            Vendortbl c = await db.Vendortbls.FindAsync(id);
+            return Ok(c);
+        }
+    }
+}
